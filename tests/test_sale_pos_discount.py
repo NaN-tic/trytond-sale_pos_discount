664448b741397a852a755cab9d6fdc35dc38300a
resteve <resteve@zikzakmedia.com>
2015-11-29 23:46:24 +0100

    Tests

--- tests/test_sale_pos_discount.py
+++ tests/test_sale_pos_discount.py
@@ -1,27 +1,14 @@
-#!/usr/bin/env python
 # This file is part of the sale_pos_discount module for Tryton.
 # The COPYRIGHT file at the top level of this repository contains the full
 # copyright notices and license terms.
-from trytond.tests.test_tryton import test_depends, test_view
-import os
-import sys
-import trytond.tests.test_tryton
 import unittest
+import trytond.tests.test_tryton
+from trytond.tests.test_tryton import ModuleTestCase
 
 
-class SalePOSDiscountTestCase(unittest.TestCase):
+class SalePOSDiscountTestCase(ModuleTestCase):
     'Test Sale POS Discount module'
-
-    def setUp(self):
-        trytond.tests.test_tryton.install_module('sale_pos_discount')
-
-    def test0005views(self):
-        'Test views'
-        test_view('sale_pos_discount')
-
-    def test0006depends(self):
-        'Test depends'
-        test_depends()
+    module = 'sale_pos_discount'
 
 
 def suite():
@@ -29,6 +16,3 @@ def suite():
     suite.addTests(unittest.TestLoader().loadTestsFromTestCase(
         SalePOSDiscountTestCase))
     return suite
-
-if __name__ == '__main__':
-    unittest.TextTestRunner(verbosity=2).run(suite())
