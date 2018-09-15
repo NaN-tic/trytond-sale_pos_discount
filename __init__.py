048dde8f73e25536514f7e171e17e093323df2ee
Raimon Esteve <raimon@nan-tic.com>
2018-09-15 18:13:27 +0200

    Migrate to py3

--- __init__.py
+++ __init__.py
@@ -2,10 +2,10 @@
 # The COPYRIGHT file at the top level of this repository contains the full
 # copyright notices and license terms.
 from trytond.pool import Pool
-from .sale import *
+from . import sale
 
 
 def register():
     Pool.register(
-        SaleLine,
+        sale.SaleLine,
         module='sale_pos_discount', type_='model')
